1644f9f4feab28f63192a3414839317bfcc834e2
eefsabine <ejalosbanos@up.edu.ph>
2019-10-10 07:12:31 +0800

    updated edit profile

---
+++ prototype/firebase.js
@@ -0,0 +1,19 @@
+src="https://www.gstatic.com/firebasejs/7.1.0/firebase-app.js";
+src="https://www.gstatic.com/firebasejs/7.1.0/firebase-database.js";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBySW1u61AJOhCnpb-SuEtzXzV0bUEbbJo",
+    authDomain: "dummy2-166.firebaseapp.com",
+    databaseURL: "https://dummy2-166.firebaseio.com",
+    projectId: "dummy2-166",
+    storageBucket: "dummy2-166.appspot.com",
+    messagingSenderId: "493903019725",
+    appId: "1:493903019725:web:d78ea74ac9b36c52b4f384"
+  };
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
+
+ //var rootRef = firebase.database().ref();
+ //if (!firebase.apps.length) {
+ //   firebase.initializeApp(config);
+//}
